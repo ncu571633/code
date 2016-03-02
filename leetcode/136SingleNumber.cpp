2ec03fd8050ee40d21d8695287c1a0aac71398d5
571633 <hechuanjun@msn.com>
2016-03-01 19:52:18 -0500

    modified:   136SingleNumber.cpp

--- leetcode/136SingleNumber.cpp
+++ leetcode/136SingleNumber.cpp
@@ -24,3 +24,23 @@ class Solution {
 		}
 }
 
+/*
+
+   */
+
+
+/*
+XOR is commutative and associative (so the order in which it's done is irrelevant).
+a number XORed with itself will always be zero.
+zero XORed with a number will be that number
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp = 0;
+        for(int i=0; i<nums.size(); i++)
+            temp ^= nums[i];
+        return temp;
+    }
+};
