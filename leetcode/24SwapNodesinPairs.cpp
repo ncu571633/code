41752e3b680aa310de34a04c9e8dec61275cbd59
571633 <hechuanjun@msn.com>
2016-03-09 22:28:29 -0500

    new file:   24SwapNodesinPairs.cpp 	new file:   62UniquePaths.cpp

---
+++ leetcode/24SwapNodesinPairs.cpp
@@ -0,0 +1,29 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be chang
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        ListNode* a = head;
+        ListNode* b = head->next;
+        a->next = swapPairs(b->next);
+        b->next = a;
+        return b;
+    }
+};
