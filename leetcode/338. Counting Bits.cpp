27941ab9416ee85556f215c36c2b1349da19a05a
571633 <hechuanjun@msn.com>
2016-03-18 21:25:51 -0400

    new file:   338. Counting Bits.cpp

---
+++ leetcode/338. Counting Bits.cpp
@@ -0,0 +1,55 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ¡Ü i ¡Ü num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+Hint:
+
+You should make use of what you have produced already.
+Divide the numbers in ranges like [2-3], [4-7], [8-15] and so on. And try to generate new range from previous.
+Or does the odd/even status of the number help you in calculating the number of 1s?
+
+*/
+
+0    0000    0
+-------------
+1    0001    1
+-------------
+2    0010    1
+3    0011    2
+-------------
+4    0100    1
+5    0101    2
+6    0110    2
+7    0111    3
+-------------
+8    1000    1
+9    1001    2
+10   1010    2
+11   1011    3
+12   1100    2
+13   1101    3
+14   1110    3
+15   1111    4
+
+
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> countBits(int num) {
+        vector<int> res{0};
+        for (int i = 1; i <= num; ++i) {
+            if (i % 2 == 0) res.push_back(res[i / 2]);
+            else res.push_back(res[i / 2] + 1);
+        }
+        return res;
+    }
+    }
+};
