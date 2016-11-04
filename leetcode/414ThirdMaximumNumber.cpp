4e1ad9a5b798f15310bbce4770cc4bcd6283f335
a <hechuanjun@msn.com>
2016-11-03 23:43:20 -0400

    new file:   414ThirdMaximumNumber.cpp 	modified:   70ClimbingStairs.cpp

---
+++ leetcode/414ThirdMaximumNumber.cpp
@@ -0,0 +1,61 @@
+/*
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+
+Example 2:
+
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+
+*/
+
+#include <vector>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    int thirdMax(vector<int> nums) {
+        set<int> s;
+        for(int i=0; i<nums.size(); i++) {
+            if(s.size()<3)
+                s.insert(nums[i]);
+            else{
+                if(*(s.begin()) < nums[i])
+                {
+                    s.erase(s.begin());
+                    s.insert(nums[i]);
+                }
+            }
+        }
+        if(s.size()<3)
+            return *(s.rend());
+        else
+            return *(s.begin());
+    }
+};
+
+int main()
+{
+    int a[] = {1,2,2,5,3,5};
+    Solution s;
+    s.thirdMax(vector<int>(a, a+sizeof(a) / sizeof(int)));
+}
--- leetcode/70ClimbingStairs.cpp
+++ leetcode/70ClimbingStairs.cpp
@@ -19,13 +19,10 @@ public:
     int climbStairs(int n) {
         if(!n || n==1)
             return 1;
-        if(n==2)
-            return 2;
-        int a = 1, b = 2;
-        for(int i=3; i<=n; i++) {
-            int t = b;
-            b = a+b;
-            a = t;
+        int a = 1, b = 1;
+        for(int i=2; i<=n; i++) {
+            a = a+b;
+            swap(a, b);
         }
         return b;
     }
