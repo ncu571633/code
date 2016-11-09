361fb7eef552425830f3269af43a198802b476f5
a <hechuanjun@msn.com>
2016-11-08 23:04:05 -0500

    deleted:    116. Populating Next Right Pointers in Each Node.cpp 	renamed:    338. Counting Bits.cpp -> 338CountingBits.cpp 	deleted:    343. Integer Break.cpp 	modified:   53MaxSubarray.cpp

--- leetcode/53MaxSubarray.cpp
+++ leetcode/53MaxSubarray.cpp
@@ -4,3 +4,19 @@
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6. 
 */
+
+
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int maxSum = INT_MIN;
+            int sum = 0;
+            for(int i=0; i<nums.size(); i++) {
+                sum += nums[i];
+                maxSum = max(sum, maxSum);
+                if(sum<0)
+                    sum=0;
+            }
+            return maxSum;
+        }
+};
