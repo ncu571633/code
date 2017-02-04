34bb5c230652c8fa51ca225e316bf26bd0ca7eec
ncu571633 <hechuanjun@msn.com>
2017-02-04 00:21:03 -0500

    modified:   189RotateArray.cpp 	new file:   278FirstBadVersion.hpp

--- leetcode/189RotateArray.cpp
+++ leetcode/189RotateArray.cpp
@@ -2,6 +2,17 @@ Rotate an array of n elements to the right by k steps.
 
 For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]. 
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> v(nums.size(), 0);
+        for(int i=0; i<nums.size(); i++) {
+            v[(i+k)%nums.size()] = nums[i];
+        }
+        nums = v;
+    }
+};
+
 
 class Solution {
 public:
