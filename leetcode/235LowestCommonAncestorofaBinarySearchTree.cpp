d20cd25487d87016eb5cde1e3b75cdf210386a5f
ncu571633 <hechuanjun@msn.com>
2017-03-26 22:18:38 -0400

    modified:   235LowestCommonAncestorofaBinarySearchTree.cpp

--- leetcode/235LowestCommonAncestorofaBinarySearchTree.cpp
+++ leetcode/235LowestCommonAncestorofaBinarySearchTree.cpp
@@ -55,3 +55,17 @@ public:
         return root;
     }
 };
+
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode parent;
+     TreeNode(int x) { val = x; }
+}
+
+
+If parent pointer is defined, then map
+Tarjan
+
