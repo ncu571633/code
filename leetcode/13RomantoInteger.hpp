000c76a700b9f866ff6f83ae60973258aaca7468
ncu571633 <hechuanjun@msn.com>
2017-04-27 23:34:10 -0400

    modified:   13RomantoInteger.hpp

--- leetcode/13RomantoInteger.hpp
+++ leetcode/13RomantoInteger.hpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        int ret;
+        int ret = 0;
         for(int i=0; i<s.size(); i++) {
             if(i>0 && m[s[i-1]]<m[s[i]]) {
                 ret = ret+m[s[i]]-2*m[s[i-1]];
