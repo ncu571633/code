5e7ac69c0bebbc2476d2d14b79ba8c217de12a08
ncu571633 <ncu571633@users.noreply.github.com>
2017-05-17 16:54:55 -0400

    Update 283MoveZeroes.cpp

--- leetcode/283MoveZeroes.cpp
+++ leetcode/283MoveZeroes.cpp
@@ -26,3 +26,17 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int i = 0, j = 0; i < nums.size(); ++i) {
+            if (nums[i]) {
+                if(i!=j)
+                    swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
