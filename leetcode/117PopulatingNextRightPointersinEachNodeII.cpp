ba3102201d9879f746ea675922c4764202fe6483
ncu571633 <hechuanjun@msn.com>
2018-01-21 00:22:22 -0500

    modified:   117PopulatingNextRightPointersinEachNodeII.cpp

--- leetcode/117PopulatingNextRightPointersinEachNodeII.cpp
+++ leetcode/117PopulatingNextRightPointersinEachNodeII.cpp
@@ -99,6 +99,30 @@ void connect(TreeLinkNode *root) {
     }
 }
 
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode* dummy = new TreeLinkNode(0), *t = dummy;
+        while(root)
+        {
+            if(root->left)
+            {
+                t->next = root->left;
+                t= t->next;
+            }
+            if(root->right)
+            {
+                t->next = root->right;
+                t= t->next;
+            }
+            root = root->next;
+            if(!root)
+            {
+                t = dummy;
+                root = dummy->next;
+                dummy->next = nullptr;
+            }
+        }
+    }
+
 int main() 
 {
     TreeLinkNode t(0);
