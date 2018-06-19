dce88dbac6fc5c47f34709d94fd8ced4380679c3
ncu571633 <hechuanjun@msn.com>
2018-06-18 23:52:40 -0400

    modified:   42TrappingRainWater.hpp

--- leetcode/42TrappingRainWater.hpp
+++ leetcode/42TrappingRainWater.hpp
@@ -23,3 +23,26 @@ public:
         return ret;
     }
 };
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n <= 2)
+            return 0;
+
+        vector<int> v(n, 0);
+        for(int i=1; i<n; i++) {
+            v[i] = max(v[i-1], height[i-1]);
+        }
+        int r = 0;
+        int ret = 0;
+        for(int i=n-2; i>=0; i--) {
+            r = max(r, height[i+1]);
+            int t = (min(v[i], r) - height[i]);
+            ret += max(t, 0);            
+        } 
+        return ret;
+    }
+};
