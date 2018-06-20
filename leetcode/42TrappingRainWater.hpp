d3e9690ff27af3da6b3b956868e6ecefd71afb16
ncu571633 <hechuanjun@msn.com>
2018-06-19 23:31:14 -0400

    modified:   42TrappingRainWater.hpp 	modified:   62UniquePaths.cpp

--- leetcode/42TrappingRainWater.hpp
+++ leetcode/42TrappingRainWater.hpp
@@ -46,3 +46,25 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, l = 0, r = height.size() - 1;
+        while (l < r) {
+            int mn = min(height[l], height[r]);
+            if (mn == height[l]) {
+                ++l;
+                while (l < r && height[l] < mn) {
+                    res += mn - height[l++];
+                }
+            } else {
+                --r;
+                while (l < r && height[r] < mn) {
+                    res += mn - height[r--];
+                }
+            }
+        }
+        return res;
+    }
+};
--- leetcode/62UniquePaths.cpp
+++ leetcode/62UniquePaths.cpp
@@ -21,3 +21,19 @@ public:
         return DP[m-1][n-1];
     }
 };
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(!m || !n)
+            return 1;
+        vector<int> v(n, 1);
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                v[j] += v[j-1]; 
+            }    
+        }
+        return v[n-1];
+    }
+};
