65a907ffc052e597ed7743de3ad87786fa480c12
ncu571633 <hechuanjun@msn.com>
2018-06-26 10:57:19 -0400

    new file:   287FindtheDuplicateNumber.cpp 	modified:   386LexicographicalNumbers.cpp 	new file:   746MinCostClimbingStairs.hpp

--- leetcode/386LexicographicalNumbers.cpp
+++ leetcode/386LexicographicalNumbers.cpp
@@ -6,41 +6,20 @@ For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
 Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000. 
 */
 
-#include <vector>
-#include <iostream>
-using namespace std;
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> v;
-        if(!n)
-            return v;
-        v.push_back(1);
-        for(int i=1; i<n; i++) {
-            int j = v[i-1];
-            if(j*10 <= n)
-            {
-                v.push_back(j*10);
-                continue;
-            }
-            if(j == n)
-                j = j/10;
-            j++;
-            while(j%10 == 0)
-                j = j/10;
+        for(int j=1; j<10 && j<=n; j++) {
             v.push_back(j);
+            int t = j;
+            while(t*10 <= n) {
+                t = t*10;
+                for(int i=0; i<10 && t+i<=n; i++)
+                    v.push_back(t+i);
+                t = v.back();
+            }
         }
         return v;
     }
 };
-
-int main()
-{
-    int n = 13;
-    Solution s;
-    vector<int> v = s.lexicalOrder(n);
-    for(int i=0; i<n; i++)
-        cout<<v[i]<<endl;
-}
-
-
