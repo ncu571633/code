1a245fe8c341d9bc1d808b893f1d4c0406573963
ncu571633 <ncu571633@users.noreply.github.com>
2020-07-15 20:44:33 -0700

    Update 1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp

--- leetcode/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
+++ leetcode/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -63,3 +63,26 @@ public:
         return ret;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ret = 0;
+        for(ListNode* p = head; p!=nullptr; p=p->next)
+        {
+            ret = ret*2 + p->val;
+        }
+        return ret;
+    }
+};
