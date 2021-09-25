be19dea0e0b3914b177cda81320f15b7cf6d158e
ncu571633 <ncu571633@users.noreply.github.com>
2021-09-24 20:39:19 -0700

    Update 905Sort_Array_By_Parity.cpp

--- leetcode/905Sort_Array_By_Parity.cpp
+++ leetcode/905Sort_Array_By_Parity.cpp
@@ -26,3 +26,26 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        
+        for(int i = 0, j = A.size()-1; i<j; )
+        {
+            while(i<j && A[i]%2==0)
+            {
+                i++;
+            }
+            while(j>i && A[j]%2!=0)
+            {
+               j--; 
+            }
+            if (i!=j)
+            {
+                swap(A[i++], A[j--]);
+            }
+        }
+        return A;
+    }
+};
