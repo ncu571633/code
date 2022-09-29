6350ad3839693da6da60ea7110f192d33f27e0ba
ncu571633 <ncu571633@users.noreply.github.com>
2022-09-28 21:20:28 -0700

    Update 21MergeTwoSortedLists.cpp

--- leetcode/21MergeTwoSortedLists.cpp
+++ leetcode/21MergeTwoSortedLists.cpp
@@ -12,19 +12,17 @@ Merge two sorted linked lists and return it as a new list. The new list should b
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(!l1)
-            return l2;
-        if(!l2)
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
         
-        if(l1->val <= l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
+        if (list1->val > list2->val)
+            return mergeTwoLists(list2, list1);
+        
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
     }
 };
 
