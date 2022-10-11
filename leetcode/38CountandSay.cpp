916f0763678ab3b767c449026f93cd267e64957d
ncu571633 <ncu571633@users.noreply.github.com>
2022-10-10 22:05:51 -0700

    Update 38CountandSay.cpp

--- leetcode/38CountandSay.cpp
+++ leetcode/38CountandSay.cpp
@@ -9,28 +9,37 @@ Given an integer n, generate the nth sequence.
 
 class Solution {
 public:
-    string getNext(string s) {
-        int count = 1;
-        string t;
-        for(int i=1; i<s.size(); i++) {
-            if(s[i]==s[i-1]) {
-                count++;
-            } else {
-                t = t+to_string(count)+string(1, s[i-1]);
-                count = 1;
-            }
-        }
-        t = t+to_string(count)+string(1, s.back());
-        return t;
-    }
     string countAndSay(int n) {
-        if(n<=0)
+        if (n <= 0)
             return "";
-        string s = "1";
-        for(int i=1; i<n; i++) {
-            cout<< s << endl;
-            s = getNext(s);
+        string ret = "1";
+        for(int i = 1; i < n; i++)
+        {
+            string temp;
+            int count = 1;
+            for(int j = 1; j <= ret.size(); j++)
+            {
+                if (j == ret.size())
+                {
+                    temp += to_string(count);
+                    temp += ret[j-1];
+                    break;
+                }
+
+                if (ret[j] == ret[j-1])
+                {
+                    count ++;
+                }
+                else
+                {
+                    temp += to_string(count);
+                    temp += ret[j-1];
+                    count = 1;
+                }
+            }
+            ret = temp;
         }
-        return s;
+
+        return ret;
     }
 };
